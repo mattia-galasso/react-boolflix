f26da72b7761036526f203b7816acbc2687d5d8b
MattiaGalasso <galasso.mattia94@gmail.com>
2026-02-24 11:44:42 +0100

    Add: Readme, Clear css file and app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,6 @@ export default function App() {
   return (
     <>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
     </>
   );
 }
